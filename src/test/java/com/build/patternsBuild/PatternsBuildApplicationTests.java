5c4ec0e9b92ecfca280119b0ae47e0432e1c3fd9
crispimluiz <crispimluiz@live.com>
2023-06-13 20:11:12 -0300

    first commit

---
+++ src/test/java/com/build/patternsBuild/PatternsBuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.build.patternsBuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatternsBuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
